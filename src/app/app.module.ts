fe61f35f7e1ff797d00ef2edcb60af96bb1d3c0d
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-06-02 12:34:24 -0400

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ShoppinglistComponent } from "./shoppinglist/shoppinglist.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
+import { RecipedetailComponent } from './recipes/recipedetail/recipedetail.component';
+import { RecipeitemComponent } from './recipelist/recipeitem/recipeitem.component';
+import { ShoppingeditComponent } from './shoppinglist/shoppingedit/shoppingedit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ShoppinglistComponent, HeaderComponent, RecipesComponent, RecipelistComponent, RecipedetailComponent, RecipeitemComponent, ShoppingeditComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
