63e35129a703a59ab8d204783311377e9e332d29
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-06-04 16:57:53 -0400

    add models, sim model data, add sections html

--- src/app/recipes/recipelist/recipelist.component.ts
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipelist',
-  templateUrl: './recipelist.component.html',
-  styleUrls: ['./recipelist.component.scss']
+  selector: "app-recipelist",
+  templateUrl: "./recipelist.component.html",
+  styleUrls: ["./recipelist.component.scss"]
 })
 export class RecipelistComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "This is a test recipe",
+      "https://cdn.pixabay.com/photo/2018/07/04/19/51/roasted-chickpeas-3516806_960_720.jpg"
+    ),
+    new Recipe(
+      "Test Recipe",
+      "This is a test recipe",
+      "https://cdn.pixabay.com/photo/2018/07/04/19/51/roasted-chickpeas-3516806_960_720.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/shoppinglist/shoppinglist.component.ts
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shoppinglist',
-  templateUrl: './shoppinglist.component.html',
-  styleUrls: ['./shoppinglist.component.scss']
+  selector: "app-shoppinglist",
+  templateUrl: "./shoppinglist.component.html",
+  styleUrls: ["./shoppinglist.component.scss"]
 })
 export class ShoppinglistComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Potato", 1)
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
